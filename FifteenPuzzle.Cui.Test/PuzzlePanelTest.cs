428e25ef9347c002fa1b0641dbbfe94730919eb7
Tana <finaltruth.shintai@gmail.com>
2018-12-06 22:30:37 +0900

    * 単体テストを追加。

---
+++ FifteenPuzzle.Cui.Test/PuzzlePanelTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FifteenPuzzle.Cui.Puzzle;
+using NUnit.Framework;
+
+namespace FifteenPuzzle.Cui.Test
+{
+    [TestFixture]
+    public class PuzzlePanelTest
+    {
+        [Test]
+        public void TestCreateNew()
+        {
+            Assert.Throws<ArgumentException>(() => new PuzzlePanel(0));
+
+            Assert.DoesNotThrow(() => new PuzzlePanel(1));
+        }
+
+        [Test]
+        public void TestNumber()
+        {
+            var p = new PuzzlePanel(1);
+            Assert.AreEqual(1, p.Number);
+        }
+
+        [Test]
+        public void TestToString()
+        {
+            var p = new PuzzlePanel(1);
+            Assert.AreEqual("[ 1]", p.ToString());
+
+            Assert.AreEqual("    ", PuzzlePanel.Free.ToString());
+        }
+    }
+}
--- FifteenPuzzle.Cui/Puzzle/PuzzlePanel.cs
+++ FifteenPuzzle.Cui/Puzzle/PuzzlePanel.cs
@@ -7,12 +7,16 @@ namespace FifteenPuzzle.Cui.Puzzle
     /// </summary>
     public class PuzzlePanel
     {
-        private const string InvalidNumberMessage = "パネルの番号をゼロ以下にすることはできません。";
+        #region テキストリソース
 
-        /// <summary>パネルの番号</summary>
-        private readonly int number;
-        /// <summary>パネルの番号を表す文字列</summary>
-        private readonly string text;
+        private const string InvalidNumberMessage = "パネルの番号をゼロ以下にすることはできません。";
+        private const string TextFormat = "[{0,2}]";
+        private const string FreePanelText = "    ";
+        private const int FreePanelNumber = 0;
+
+        #endregion
+
+        #region 固有のインスタンス
 
         /// <summary>
         /// 空白パネルを取得します。
@@ -23,22 +27,30 @@ namespace FifteenPuzzle.Cui.Puzzle
         /// <see cref="PuzzlePanel"/> の新しいインスタンスを生成します。
         /// </summary>
         /// <param name="number"></param>
-        public PuzzlePanel(int number)
+        private PuzzlePanel()
         {
-            if (number <= 0) { throw new ArgumentException(InvalidNumberMessage, nameof(number)); }
-
-            this.number = number;
-            text = $"[{number,2}]";
+            Number = FreePanelNumber;
+            text = FreePanelText;
         }
+
+        #endregion
+
+        /// <summary>パネルの番号を表す文字列</summary>
+        private readonly string text;
+
+        /// <summary>パネルの番号を取得します。</summary>
+        public int Number { get; }
 
         /// <summary>
         /// <see cref="PuzzlePanel"/> の新しいインスタンスを生成します。
         /// </summary>
         /// <param name="number"></param>
-        private PuzzlePanel()
+        public PuzzlePanel(int number)
         {
-            number = 0;
-            text = "[  ]";
+            if (number <= 0) { throw new ArgumentException(InvalidNumberMessage, nameof(number)); }
+
+            Number = number;
+            text = string.Format(TextFormat, number);
         }
 
         /// <summary>
